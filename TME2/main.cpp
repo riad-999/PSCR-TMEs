a2e574d4824dc76e7a8ea60988b1da80b68d0e29
riad-999 <felihriad5@gmail.com>
2023-10-04 00:34:44 +0200

    tme2

--- TME2/main.cpp
+++ TME2/main.cpp
@@ -7,7 +7,9 @@
 #include <climits>
 #include <functional>
 #include <cassert>
+#include <algorithm>
 
+int zero = 0;
 
 
 using namespace std;
@@ -90,7 +92,7 @@ void word_count(const string& searched_word) {
 	input.close();
 
 	cout << words.size() << endl;
-	cout << words[3] << endl;
+	// cout << words[3] << endl;
 
 	cout << "Finished Parsing War and Peace" << endl;
 
@@ -152,13 +154,22 @@ class HashMap {
 	size_t size;
 
 	public: 
-		HashMap(): size(4092), tab(size) {}
+		HashMap(){
+			size = 256;
+			tab.resize(size);
+			for (size_t i = 0; i < size; i++) {
+				List list = List();
+				tab[i] = list;
+			}
+			// exit(0);
+		}
 		int& get(string key) {
 			size_t index = std::hash<string>{}(key) % size;
-			List list = tab[index];
+			List& list = tab[index];
 			if (list.empty()) {
-				std::cout << "no such key as: " << key << std::endl;
-				assert(false);
+				return zero;
+				// std::cout << "no such key as: " << key << std::endl;
+				// assert(false);
 			}
 			Chainon* ptr = list.tete;
 			while(ptr) {
@@ -166,14 +177,15 @@ class HashMap {
 					return ptr->data.value;
 				ptr = ptr->next;
 			}
-			std::cout << "no such key as: " << key << std::endl;
-			assert(false);
+			return zero;
+			// std::cout << "no such key as: " << key << std::endl;
+			// assert(false);
 		}
 		void add(string key) {
 			size_t index = std::hash<string>{}(key) % size;
-			List list = tab[index];
+			List& list = tab[index];
 			if(list.empty()) {
-				Pair pair = Pair(key, 1);
+				Pair pair(key, 1);
 				Chainon* ch = new Chainon(pair);
 				list.push_back(*ch);
 			}
@@ -186,7 +198,7 @@ class HashMap {
 					}
 					ptr = ptr->next;
 				}
-				Pair pair = Pair(key, 1);
+				Pair pair(key, 1);
 				Chainon* ch = new Chainon(pair);
 				list.push_back(*ch);
 			}
@@ -197,10 +209,86 @@ class HashMap {
 		int& operator[](string key) {
 			return get(key);
 		}
+		List& operator[](size_t index) {
+			return tab[index];
+		}
+		void test() {
+			Chainon* ptr = tab[100].tete;
+			while (ptr) {
+				std::cout << ptr->data.key << " = " << ptr->data.value << std::endl;
+				ptr = ptr->next;
+			}
+		}
 };
 
+void word_count_distinct_with_hashmap(HashMap& hash) {
+	ifstream input = ifstream("./WarAndPeace.txt");
+	// HashMap hash;
+	// exit(0);
+	int i = 0;
+	// exit(0);
+	auto start = steady_clock::now();
+	cout << "Parsing War and Peace" << endl;
+
+	// prochain mot lu
+	string word;
+	// une regex qui reconnait les caractères anormaux (négation des lettres)
+	regex re( R"([^a-zA-Z])");
+
+	while (input >> word) {
+		// élimine la ponctuation et les caractères spéciaux
+		word = regex_replace ( word, re, "");
+		// passe en lowercase
+		transform(word.begin(),word.end(),word.begin(),::tolower);
+		// exit(0);
+		hash.add(word);
+		// exit(0);
+		i++;
+		if (i % 100 == 0) {
+			std::cout << "mot: " << word << ", nbr: " << hash[word] << std::endl;
+		}
+	}
+	input.close();
+
+	cout << "Finished Parsing War and Peace" << endl;
+
+	auto end = steady_clock::now();
+    cout << "Parsing took "
+              << duration_cast<milliseconds>(end - start).count()
+              << "ms.\n";
+
+
+	cout << hash["war"] << ", " << hash["peace"] << ", " << hash["toto"] << endl;
+}
+
+void test() {
+
+}
+
+void hashmap_to_vector(HashMap& hash, vector<Pair>& vect) {
+	for (size_t i = 0; i < hash.get_size(); i++) {
+		Chainon* ptr = hash[i].tete;
+		while(ptr) {
+			vect.push_back(ptr->data);
+			ptr = ptr->next;
+		}
+	}
+}
+
+void sort_vector(vector<Pair>& vect) {
+	std::sort(vect.begin(), vect.end(), [] (const Pair& p1, const Pair& p2) {
+		return p1.value > p2.value;
+	});
+	for (int i = 0; i < 10; i++) {
+		cout << i + 1 << ") " << vect[i].key << " = " << vect[i].value << endl;
+	}
+}
 
 int main () {
-	// word_count_distinct();
+	vector<Pair> vect;
+	HashMap hash;
+	word_count_distinct_with_hashmap(hash);
+	hashmap_to_vector(hash, vect);
+	sort_vector(vect);
     return 0;
 }
